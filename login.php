d355eff89deb75f196b49acdcda62f40f1169056
Abass LAkiss <125732631+Lakiss313@users.noreply.github.com>
2024-11-03 17:03:18 +0200

    Update login.php

--- login.php
+++ login.php
@@ -5,7 +5,7 @@ $password = "lakiss@1@2@3";
 $dbname = "waf";
 // Create connection
 $conn = mysqli_init();
-#mysqli_ssl_set($conn, NULL, NULL, "/home/site/wwwroot/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+mysqli_ssl_set($conn, NULL, NULL, "/home/site/wwwroot/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
 // Connect with SSL verification
 #mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 // Check connection
