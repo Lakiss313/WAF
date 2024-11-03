91da6308312ff9d0c37a19a499b1cb21d1542bce
Abass LAkiss <125732631+Lakiss313@users.noreply.github.com>
2024-11-03 15:03:22 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,40 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-</head>
-<body>
-    <h2>Login Page</h2>
-    <form action="login.php" method="post">
-        <label for="username">Username:</label>
-        <input type="text" id="username" name="username" required><br><br>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" required><br><br>
-        <button type="submit">Login</button>
-    </form>
-</body>
-</html>
+#<!DOCTYPE html>
+#<html lang="en">
+#<head>
+#    <meta charset="UTF-8">
+#    <title>Login</title>
+#</head>
+#<body>
+#    <h2>Login Page</h2>
+#    <form action="login.php" method="post">
+#        <label for="username">Username:</label>
+#        <input type="text" id="username" name="username" required><br><br>
+#        <label for="password">Password:</label>
+#        <input type="password" id="password" name="password" required><br><br>
+#        <button type="submit">Login</button>
+#    </form>
+#</body>
+#</html>
+
+
+<?php
+$servername = "abasswaf.mysql.database.azure.com";
+$username = "Abass@abasswaf";
+$password = "lakiss@1@2@3";
+$dbname = "waf";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
+$conn->close();
+?>
+
+
+
+
