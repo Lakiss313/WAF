5f2df76ec1dffc6c6f098cdeec6bd73ab8643c5e
Abass LAkiss <125732631+Lakiss313@users.noreply.github.com>
2024-11-03 16:51:57 +0200

    Update login.php

--- login.php
+++ login.php
@@ -12,9 +12,7 @@ mysqli_ssl_set($conn, NULL, NULL, "/home/site/wwwroot/BaltimoreCyberTrustRoot.cr
 mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
 
 // Check connection
-if (mysqli_connect_errno()) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+
 echo "Connected successfully";
 
 $conn->close();
