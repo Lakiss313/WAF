e70c5aba123c1b9cda22ff40e2021d54ae765079
Abass LAkiss <125732631+Lakiss313@users.noreply.github.com>
2024-11-03 17:10:05 +0200

    Update login.php

--- login.php
+++ login.php
@@ -3,15 +3,18 @@ $servername = "abasswaf.mysql.database.azure.com";
 $username = "Abass@abasswaf";
 $password = "lakiss@1@2@3";
 $dbname = "waf";
-// Create connection
+
 $conn = mysqli_init();
 mysqli_ssl_set($conn, NULL, NULL, "/home/site/wwwroot/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
-// Connect with SSL verification
-mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
-// Check connection
-echo "Connected successfully";
-$conn->close();
+
+if (mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL)) {
+    echo "Connected successfully";
+} else {
+    echo "Connection failed: " . mysqli_connect_error();
+}
+mysqli_close($conn);
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
