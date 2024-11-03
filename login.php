563a1fae9465acaccd3bb11164b306cf065501df
Abass LAkiss <125732631+Lakiss313@users.noreply.github.com>
2024-11-03 16:27:08 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,16 +1,13 @@
 <?php
-$servername = "abasswaf.mysql.database.azure.com";
-$username = "Abass@abasswaf";
-$password = "lakiss@1@2@3";
-$dbname = "waf";
+$servername = getenv('abasswaf.mysql.database.azure.com');
+$username = getenv('Abass@abasswaf');
+$password = getenv('lakiss@1@2@3');
+$dbname = getenv('waf');
 
 $conn = mysqli_init();
-// Set the SSL certificate
-mysqli_ssl_set($conn, NULL, NULL, "/path/to/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
-// Attempt to connect
+mysqli_ssl_set($conn, NULL, NULL, "/home/site/wwwroot/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
 
-// Check for connection errors
 if (mysqli_connect_errno()) {
     die("Connection failed: " . mysqli_connect_error());
 } else {
@@ -18,3 +15,4 @@ if (mysqli_connect_errno()) {
 }
 
 $conn->close();
+?>
