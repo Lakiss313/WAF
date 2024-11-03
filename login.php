aca338f8b9d51b5ab7a423a4ed02ca30715c434a
Abass LAkiss <125732631+Lakiss313@users.noreply.github.com>
2024-11-03 16:11:43 +0200

    Update login.php

--- login.php
+++ login.php
@@ -4,14 +4,17 @@ $username = "Abass@abasswaf";
 $password = "lakiss@1@2@3";
 $dbname = "waf";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname, 3306);
+$conn = mysqli_init();
+// Set the SSL certificate
+mysqli_ssl_set($conn, NULL, NULL, "/path/to/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
+// Attempt to connect
+mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+// Check for connection errors
+if (mysqli_connect_errno()) {
+    die("Connection failed: " . mysqli_connect_error());
+} else {
+    echo "Connected successfully";
 }
-echo "Connected successfully";
 
 $conn->close();
-?>
