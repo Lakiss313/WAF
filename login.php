f6f73eb454daa57e17a15c4e12cef267b1d201cd
Abass LAkiss <125732631+Lakiss313@users.noreply.github.com>
2024-11-03 16:40:46 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,18 +1,20 @@
 <?php
-$servername = getenv('abasswaf.mysql.database.azure.com');
-$username = getenv('Abass@abasswaf');
-$password = getenv('lakiss@1@2@3');
-$dbname = getenv('waf');
+$servername = "abasswaf.mysql.database.azure.com";
+$username = "Abass@abasswaf";
+$password = "lakiss@1@2@3";
+$dbname = "waf";
 
+// Create connection
 $conn = mysqli_init();
 mysqli_ssl_set($conn, NULL, NULL, "/home/site/wwwroot/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL);
 mysqli_real_connect($conn, $servername, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL);
 
+// Check connection
 if (mysqli_connect_errno()) {
     die("Connection failed: " . mysqli_connect_error());
-} else {
-    echo "Connected successfully";
 }
+echo "Connected successfully";
 
 $conn->close();
 ?>
+
